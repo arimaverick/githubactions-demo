365eebcc3682f62dd7d8102d5459b4d1be4f0edc
arimaverick <arimaverick@gmail.com>
2021-04-23 22:28:41 +0100

    added workflow

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -4,7 +4,7 @@ echo $json_response
 total_cnt=`echo $json_response | jq .total_count`
 echo $total_cnt
 total_cnt_1=`expr $total_cnt - 1`
-echo "Total Runners: $total_cnt_1"
+echo "Total Runners I have: $total_cnt_1"
 total_runners_cnt=`expr $total_cnt - 1`
 echo $total_runners_cnt
 for i in {0..$total_runners_cnt}
