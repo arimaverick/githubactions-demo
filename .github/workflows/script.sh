c8d54f59ea4ccae045f61ac50d77da13265fab2c
arimaverick <arimaverick@gmail.com>
2021-04-24 00:48:14 +0100

    cleanup

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,21 +1,25 @@
 export GITHUB_PAT=`echo ${{ secrets.RUNNER_PAT }}`
 json_response=$(curl -sX GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_PAT" https://api.github.com/repos/arimaverick/terraform-repo/actions/runners)
-echo $json_response
 total_cnt=`echo $json_response | jq .total_count`
-echo $total_cnt
-total_cnt_1=`expr $total_cnt - 1`
-echo "Total Runners I have: $total_cnt_1"
+echo "Total Runners: $total_cnt"
 total_runners_cnt=`expr $total_cnt - 1`
 echo $total_runners_cnt
-for i in {0..$total_runners_cnt}
+a=0
+for i in $(seq 0 1 $total_runners_cnt)
 do
   echo "Checking runner $i"
-  status=`echo $json_response | jq .runners[$i].status`
-  if [ $status == "offline"]
+  status=`echo $json_response | jq -r .runners[$i].status`
+  #echo $status
+  if [ $status == 'offline' ]
   then
-    runner_id=`echo $json_response | jq .runners[$i].id
+    runner_id=`echo $json_response | jq .runners[$i].id`
     curl -sX DELETE -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_PAT" https://api.github.com/repos/arimaverick/terraform-repo/actions/runners/$runner_id
-  else
-    echo "Great! There is no offline runners today" 
+    a=`expr $a + 1`
   fi 
 done
+if [ $a -eq 0 ]
+then 
+    echo "Great! There are NO OFFLINE runner today" 
+else
+    echo "Total OFFLINE runners deleted: $a"
+fi 
