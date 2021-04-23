5987dc340b416232520885962a31367279e2ac4c
arimaverick <arimaverick@gmail.com>
2021-04-23 22:16:14 +0100

    added workflow

--- .github/workflows/script.sh
+++ .github/workflows/script.sh
@@ -1,11 +1,11 @@
 export GITHUB_PAT=`echo ${{ secrets.RUNNER_PAT }}`
 json_response=$(curl -sX GET -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_PAT" https://api.github.com/repos/arimaverick/terraform-repo/actions/runners)
 echo $json_response
-total_runners_cnt=`echo $json_response | jq .total_count`
-echo "Total Runners: $total_runners_cnt"
-total_runners_cnt=`expr $total_runners_cnt - 1`
+total_cnt=`echo $json_response | jq .total_count`
+echo "Total Runners: $total_cnt"
+total_runners_cnt=`expr $total_cnt - 1`
 echo $total_runners_cnt
-for i in `seq 0 $total_runners_cnt`
+for i in {0..$total_runners_cnt}
 do
   echo "Checking runner $i"
   status=`echo $json_response | jq .runners[$i].status`
